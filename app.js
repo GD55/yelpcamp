54ff0b9a63457a79fa8a226852e651421f318716
Gaurav Dhingra <dhingrag11@gmail.com>
2020-05-19 01:22:03 +0530

    added .env file support

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 var express = require("express"),
 	app = express(),
-    bodyParser = require("body-parser"),
+	bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
 	passport = require("passport"),
 	LocalStatergy = require("passport-local"),
 	flash = require("connect-flash"),
 	methodOverride = require("method-override"),
 	expressSanitizer = require("express-sanitizer"),
-    Campground = require("./models/campground"),
+	Campground = require("./models/campground"),
 	Comment = require("./models/comment"),
 	User = require("./models/user"),
 	seedDB = require("./seeds"),
 	commentRoutes = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	authRoutes = require("./routes/index");
-	
-mongoose.connect(process.env.DATABASEURL,{useNewUrlParser: true});
 
-app.use(bodyParser.urlencoded({extended:true}));
+require('dotenv').config({ path: __dirname + '/.env' })
+
+mongoose.connect(process.env['DATABASEURL'], { useNewUrlParser: true });
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
@@ -37,16 +39,16 @@ passport.use(new LocalStatergy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
 	res.locals.currentUser = req.user;
 	res.locals.error = req.flash("error");
 	res.locals.success = req.flash("success");
 	next();
 });
 
-app.use("/campgrounds",campgroundRoutes);
-app.use("/campgrounds/:id/comments",commentRoutes);
-app.use("/",authRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/", authRoutes);
 
 // Campground.create(
 // 	{
@@ -73,6 +75,6 @@ app.use("/",authRoutes);
 // 		{name: "Mountain Groot",img: "http://hillsidecampsite.co.uk/wp-content/uploads/2018/04/Webp.net-compress-image.jpg"}
 // ]
 
-app.listen(process.env.PORT,process.env.IP,function(){
-	console.log("YelpCamp server started on port 3000");
+app.listen(process.env.PORT || 3000, process.env.IP, function () {
+	console.log("YelpCamp server started on port " + process.env.PORT || 3000);
 });
